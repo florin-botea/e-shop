1ffc53d1c77186886c7ec0fb3dc2d61b91e8fd04
Florin Botea <php.templates.git@gmail.com>
2023-08-20 19:01:33 +0300

    pus

--- admin/Providers/ViewServiceProvider.php
+++ admin/Providers/ViewServiceProvider.php
@@ -16,9 +16,35 @@ class ViewServiceProvider extends ServiceProvider
     public function boot(PhpTemplates $templates)
     {
         $config = $templates->getConfig();
-        
+
         $config->helper('t', function($t, $params) {
             return $params;
+        });
+
+        $config->setDirective('model', function($node, $params) {
+            if (!$node->hasAttribute('label') && !$node->hasAttribute(':label')) {
+                $node->setAttribute('label', "{t '$params'}");
+            }
+
+            if (!$node->hasAttribute('name') && !$node->hasAttribute(':name')) {
+                $parts = explode('.', $params);
+                $name = '';
+                foreach ($parts as $i => $part) {
+                    if ($i > 0) {
+                        $part = "[$part]";
+                    }
+                    $name .= $part;
+                }
+                $node->setAttribute('name', $name);
+            }
+
+            if (!$node->hasAttribute('id') && !$node->hasAttribute(':id')) {
+                $node->setAttribute('id', str_replace('.', '-', $params));
+            }
+
+            if (!$node->hasAttribute('value') && !$node->hasAttribute(':value')) {
+                $node->setAttribute(':value', 123); // TODO
+            }
         });
     }
 }
